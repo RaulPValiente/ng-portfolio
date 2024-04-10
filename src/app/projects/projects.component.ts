971ca02d80b75267a45e24c919c37663274630f6
Raúl <raulperezvaliente95@gmail.com>
2024-04-10 15:23:50 +0200

    Project + Tech design

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,6 +10,11 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
-  loginDescription = "A user-friendly login system with registration, constructed using Python and Flask framework, featuring seamless authentication and user management via Jinja templates, enhancing user experience and security.";
-
+  loginDescription = "Developed with Python and Flask, this authentication system employs SQLite for efficient data management, ensuring security and user convenience.";
+  twDatabaseDescriprion = 
+  "Constructed through MySQL and MySQL Workbench, this project mirrors Twitter's database, offering efficient data storage and management for enhanced functionality."
+  gifsDescription = "Crafted with Angular, Typescript, and Bootstrap, this project integrates the Giphy API to dynamically showcase GIFs on screen, delivering an engaging user experience.";
+  portfolioDescription = "Designed using Angular, Typescript, and Bootstrap, this portfolio serves as a showcase of my skills and expertise, aimed at introducing myself and highlighting my capabilities.";
+  countriesDescription = "Web application crafted with Angular, TypeScript, and Bootstrap, offering users real-time insights into diverse countries worldwide. It utilizes an API containing country information.";
+  portfolioSvelteDescription = "A sleek portfolio template created using Svelte, Bootstrap, and JavaScript. It elegantly showcases the user's work, providing a dynamic and visually appealing presentation."
 }
