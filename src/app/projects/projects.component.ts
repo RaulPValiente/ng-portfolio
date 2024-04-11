e2df0e0bf4d938b0a52deaeccfd0439361150665
Raúl <raulperezvaliente95@gmail.com>
2024-04-11 14:00:42 +0200

    Bugs fix

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,11 +10,11 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
-  loginDescription = "Developed with Python and Flask, this authentication system employs SQLite for efficient data management, ensuring security and user convenience.";
+  loginDescription = "Developed with Python and Flask, this authentication system employs SQLite for efficient data management. Additionally, it utilizes Jinja templating for dynamic user interfaces.";
   twDatabaseDescriprion = 
   "Constructed through MySQL and MySQL Workbench, this project mirrors Twitter's database, offering efficient data storage and management for enhanced functionality."
   gifsDescription = "Crafted with Angular, Typescript, and Bootstrap, this project integrates the Giphy API to dynamically showcase GIFs on screen, delivering an engaging user experience.";
   portfolioDescription = "Designed using Angular, Typescript, and Bootstrap, this portfolio serves as a showcase of my skills and expertise, aimed at introducing myself and highlighting my capabilities.";
   countriesDescription = "Web application crafted with Angular, TypeScript, and Bootstrap, offering users real-time insights into diverse countries worldwide. It utilizes an API containing country information.";
-  portfolioSvelteDescription = "A sleek portfolio template created using Svelte, Bootstrap, and JavaScript. It elegantly showcases the user's work, providing a dynamic and visually appealing presentation."
+  mergerDescription = "Developed in Python, this project utilizes a PDF manipulation library to seamlessly merge multiple PDF files. Users can combine files into a single document, streamlining PDF management."
 }
