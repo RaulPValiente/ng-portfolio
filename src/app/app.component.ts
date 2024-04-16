e1bdee2eaafe5006a733065c003b21e1bc398725
Raúl <raulperezvaliente95@gmail.com>
2024-04-16 12:32:06 +0200

    Project links + footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { ProjectsComponent } from './projects/projects.component';
 import { EducationComponent } from './education/education.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, TechnologiesComponent, ProjectsComponent, EducationComponent, HomeComponent, ContactComponent ],
+  imports: [RouterOutlet, NavbarComponent, TechnologiesComponent, ProjectsComponent, EducationComponent, HomeComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
